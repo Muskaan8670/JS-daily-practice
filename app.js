90acb0924cfbe8521d24c2df376e31e1ec711096
Muskaan <hasratnabiha@gmail.com>
2026-01-18 22:24:32 +0530

    implement Instagram-style like feature with floating hearts

--- app.js
+++ app.js
@@ -1,26 +1,76 @@
-let btn = document.querySelector('button');
-let h1 = document.querySelector('h1');
-let inner = document.querySelector('.inner');
-let c =0;
+let img = document.querySelector('img');
+let love = document.querySelector('i');
+let like = document.querySelector('#like');
 
-btn.addEventListener('click', function(){
-    btn.innerHTML="Downloading...";
-    btn.style.pointerEvents = 'none';
-    let stop = setInterval(() => {
-      c++;
-     h1.innerHTML = c+'%';
-     inner.style.width = c+'%';
-     console.log(c);
-    },50);
+let isLiked = false;
 
-    setTimeout(() => {
-      clearInterval(stop);
-      btn.style.opacity=0.5;
-      btn.innerHTML="Download again!";
-      btn.style.pointerEvents = 'auto';
-      btn.style.opacity = 1;
-      c = 0;
-    },5000);
+img.addEventListener('dblclick', function (e) {
 
-     
-})
+  if (!isLiked) {
+    isLiked = true;
+
+    //like button
+    like.style.background ="linear-gradient(45deg, #fd1d1d, #e1306c, #c13584, #833ab4)";
+    like.style.webkitBackgroundClip = "text";
+    like.style.webkitTextFillColor = "transparent";
+    //create hearts
+    const heart1 = document.createElement('i'); 
+    heart1.classList.add('ri-heart-3-fill', 'love', 'animate-heart');
+    
+    img.parentElement.appendChild(heart1);
+    const createHeart = (size, left) => {
+    const h = document.createElement('i');
+    h.classList.add('ri-heart-3-fill', 'love', 'animate-heart');
+    h.style.cssText = `
+    font-size:${size}px;
+    rotate:360deg;
+    left:${left}; 
+    top:50%;`;
+    return h;
+   };
+
+img.parentElement.append(
+  createHeart(50, '30%'),
+  createHeart(50, '60%')
+);
+
+
+    [heart1, heart2, heart3].forEach(heart => {
+    heart.addEventListener('animationend', () => {
+      heart.remove();
+    });
+  });
+
+
+  } else {
+    isLiked = false;
+
+    like.style.background = "none";
+    like.style.webkitTextFillColor = "black";
+  }
+});
+
+
+
+// img.addEventListener('dblclick',function(){
+//   if(isLiked === false){
+//      console.log("Love");
+//   love.style.opacity = 1;
+//   love.style.scale = '1';
+//   love.style.rotate = '0deg';
+
+//   love.classList.add("animate-heart");
+
+//   // setTimeout(() => love.remove(), 1500);
+
+//   like.style.background ="linear-gradient(45deg, #fd1d1d, #e1306c, #c13584, #833ab4)";
+//   like.style.webkitBackgroundClip = "text";
+//   like.style.webkitTextFillColor = "transparent";
+
+//   isLiked = true;
+//   }
+//   else{
+//       like.style.color = 'black';
+//       isLiked = false;
+//   }
+// })
