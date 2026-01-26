fe3f782e1504053314a6708118b65cfd15dda0ce
Muskaan <hasratnabiha@gmail.com>
2026-01-26 18:38:08 +0530

    CSS radial-gradient × JS mousemove

--- app.js
+++ app.js
@@ -1,66 +1,30 @@
-let main = document.querySelector('main');
-let cursor = document.querySelector('#Jerry');
-let h2 = document.querySelector('h2');
-let tom = document.querySelector('#Tom');
-let speech = document.querySelector('.Speech img');
+let cursor = document.querySelector('#cursor');
+let box = document.querySelector('.box');
 
-function getTomCenter(el){
-    let rect = el.getBoundingClientRect();
-    return {
-    x: rect.left + rect.width / 2,
-    y: rect.top + rect.height / 2
-  };
-}
+let x=0, y=0;
+addEventListener('mousemove',(e)=>{
+    console.log(e.clientX,e.clientY);
 
-let JerryNear = false;
+    x = e.clientX ;
+    y = e.clientY ;
 
-main.addEventListener('mousemove', function(e){
-   // move Jerry
-  cursor.style.left = e.clientX + 'px';
-  cursor.style.top = e.clientY + 'px';
+    console.log(x,y);
 
-   let tomCenter = getTomCenter(tom);
-   console.log(`Tom's position ${tomCenter.x}  ${tomCenter.y}`);
+    box.style.setProperty('--x', `${x}px`);
+    box.style.setProperty('--y', `${y}px`);
 
-   let dx = e.clientX - tomCenter.x;
-   let dy = e.clientY - tomCenter.y;
-   console.log(`Jerry's X position ${dx} $ Y position ${dy}`);
+    // document.body.style.setProperty('--x', e.clientX + 'px');
+    // document.body.style.setProperty('--y', e.clientY + 'px');
 
-   let distance = Math.sqrt(dx * dx + dy * dy);
-   console.log(`Distance ${distance}`);
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
 
-   if (distance < 180) {
-      JerryNear = true;
-      console.log("if");
-      h2.innerText = "HEY! Don’t come near me 😡";
-      speech.style.opacity = '1';
-      h2.style.opacity = '1';
-
-   } else if (distance < 300 && JerryNear) {
-      console.log("if else");
-      h2.innerText = "Good. And don’t come again 😤";
-      speech.style.opacity = '1';
-      h2.style.opacity = '1';
-
-   } else {
-      console.log("else");
-      h2.style.opacity = '0'; 
-      speech.style.opacity = '0';
-      JerryNear = false;
-   }
-
-})
-
-// tom.addEventListener('mouseenter',function(){
-//     console.log("😠");
-//     h2.innerHTML = 'HEY! Don’t come near me 😠';
-//     speech.style.opacity = '1';
-//     h2.style.opacity = '1';
-// })
-
-// tom.addEventListener('mouseleave',function(){
-//     console.log("😤");
-//     h2.innerHTML = 'Good. And don’t come again 😤';
-//     speech.style.opacity = '1';
-//     h2.style.opacity = '1';
-// })
+    if(e.clientY >=164 && e.clientY <= 200 && e.clientX >= 550 && e.clientX <= 800){
+         box.style.background = 'radial-gradient(300px at var(--x) var(--y), rgba(198, 197, 197, 0) , rgb(0, 0, 0))';
+         cursor.style.transform = 'translate(200%,200%)';            
+    }
+    else{
+        box.style.background = 'radial-gradient(150px at var(--x) var(--y), rgba(198, 197, 197, 0) , rgb(0, 0, 0))';
+        cursor.style.transform = 'translate(90%,90%)';
+    }
+});
